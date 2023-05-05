d65fbcc41e01560de2b92eded146dbacdce80592
akanavalau1 <artsiom_kanavalau@epam.com>
2023-05-05 20:48:29 +0200

    commit_gatling

--- gatling-perf-test/src/test/scala/simulations/PerfTestSimulation.scala
+++ gatling-perf-test/src/test/scala/simulations/PerfTestSimulation.scala
@@ -13,7 +13,7 @@ class PerfTestSimulation extends Simulation {
   val users: Int = System.getProperty("users","1").toInt
   val url: String = System.getProperty("baseUrl", BaseHelpers.baseUrl)
   val duration: FiniteDuration = System.getProperty("duration", "1").toInt.seconds
-  val rampUp: FiniteDuration = System.getProperty("rampUp", "1").toInt.seconds
+  //val rampUp: FiniteDuration = System.getProperty("rampUp", "1").toInt.seconds
 
 
   //mvn gatling:test
