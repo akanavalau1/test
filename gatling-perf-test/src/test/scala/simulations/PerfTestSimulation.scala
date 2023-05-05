6e4aab0926b7f3f50e6a8ceba45e7fcf6522d497
akanavalau1 <artsiom_kanavalau@epam.com>
2023-05-05 19:25:17 +0200

    commit_gatling#8

--- gatling-perf-test/src/test/scala/simulations/PerfTestSimulation.scala
+++ gatling-perf-test/src/test/scala/simulations/PerfTestSimulation.scala
@@ -13,14 +13,15 @@ class PerfTestSimulation extends Simulation {
   val users: Int = System.getProperty("users","1").toInt
   val url: String = System.getProperty("baseUrl", BaseHelpers.baseUrl)
   val duration: FiniteDuration = System.getProperty("duration", "1").toInt.seconds
+  val rampUp: FiniteDuration = System.getProperty("rampUp", "1").toInt.seconds
 
 
   //mvn gatling:test
 
   val httpConf: HttpProtocolBuilder = http.baseUrl(baseUrl)
   setUp(
-    scnShopiezer.inject(constantUsersPerSec(users) during(duration)
-    //scnShopiezer.inject(rampUsers(numUsers) during(duration)
+    //scnShopiezer.inject(constantUsersPerSec(users) during(duration)
+    scnShopiezer.inject(rampUsers(users) during(rampUp)
 
   ).protocols(httpConf)
 
