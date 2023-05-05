282aa6f19fd0f265755d29f731b1ef86b1b4c283
akanavalau1 <artsiom_kanavalau@epam.com>
2023-05-05 20:55:25 +0200

    commit_gatling

--- gatling-perf-test/src/test/scala/simulations/PerfTestSimulation.scala
+++ gatling-perf-test/src/test/scala/simulations/PerfTestSimulation.scala
@@ -20,7 +20,7 @@ class PerfTestSimulation extends Simulation {
 
   val httpConf: HttpProtocolBuilder = http.baseUrl(baseUrl)
   setUp(
-    scnShopiezer.inject(constantUsersPerSec(users) during(duration)
+    scnShopiezer.inject(atOnceUsers(users)
     //scnShopiezer.inject(rampUsersPerSec(1).to(users).during(rampUp).randomized
 
   ).protocols(httpConf)
